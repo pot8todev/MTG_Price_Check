ddb1f9cdd686fb02feee9dbb6015902944a1e092
pot8todev <pedrohjesus234@gmail.com>
2026-08-21 01:09:45 -0300

    🚧 json_write modularized

--- TODO.md
+++ TODO.md
@@ -1,20 +1,28 @@
 # TODO
+<!--toc:start-->
+- [TODO](#todo)
+  - [🔴 High Priority](#🔴-high-priority)
+  - [🟡 Medium Priority](#🟡-medium-priority)
+  - [🟢 Low Priority](#🟢-low-priority)
+  - [🐛 Bugs](#🐛-bugs)
+  - [💡 Ideas](#💡-ideas)
+  - [✅ Completed](#completed)
+<!--toc:end-->
 
 ## 🔴 High Priority
 
 - [ ] do algorithim of best_stores_order
-    - [x] diversity parameter working
-    - [ ] pick the x more diverse stores
-    - [ ] check the orders required quantity
+    > - [x] diversity parameter working
+    >- [ ] pick the x more diverse stores
+    >- [ ] check the orders required quantity
 
 ## 🟡 Medium Priority
-   - [ ] parar de pegar lojas quando o preco ficar mais alto
-   - [ ]
 
+- [ ] parar de pegar lojas quando o preco ficar mais alto
 
 ## 🟢 Low Priority
 
-Clean up unused imports
+- [ ] Clean up unused imports
 - [ ] Improve scraper performance by using google collab
 - [ ] Improve variable names
 - [ ] fetch_deck closes driver instead of using the same one
@@ -24,11 +32,11 @@ Clean up unused imports
 
 ## 🐛 Bugs
 
-
 ## 💡 Ideas
 
 - [ ] Run multiple card scrapes concurrently
 - [ ] make the wish_deck input more generic
+- [ ] logica fuzzy para analise do resultado final
 
 ## ✅ Completed
 
--- index.py
+++ index.py
@@ -2,20 +2,53 @@ import json
 
 from pathlib import Path
 from dataclasses import asdict
+from re import S
 from scraper.scraper import get_stores_data
 from setup.deck_setup import wish_deck
 from setup.driver_setup import Scraper
 from setup.aux_functions import start_timer, mark_timer, sanitize_store_name
+from setup.objects.classes import Store,Stock,Card
 
     
 
 
     
+
+def sort_stores_by_diversity(stores: list[Store]) -> Store:
+    return sorted(stores, key=lambda store: store.diversity, reverse=True)
+
+def write_json(store:Store, json_path):
+    
+    stock = [asdict(item) for item in store.stock]
+    #if already exists a json with this stores name, then:
+    if json_path.exists():
+        with open(json_path, "r", encoding="utf-8") as f:
+            data = json.load(f)
+            if stock:
+                #if a store is called for having more than 1 card, inc their diversity
+                data["diversity"] += 1
+                #if it has the whole order os wish_deck, mark it
+                # if has_the_whole_card_order(wish_card_name,wish_deck[wish_card_name], store):
+        data["stock"].extend(stock)
+
+    else:
+        data ={
+            "name": store.name,
+            "address": store.address,
+            "tel_num":store.tel_num,
+            "showcase_url": store.showcase_url,
+            "diversity": 1,
+            "stock": stock
+        }
+    with open(json_path, "w", encoding="utf-8") as f:
+        json.dump(data, f, indent=4, ensure_ascii=False)
+    return data
 
 start = start_timer()
 
 out_path = Path("out")
 
+all_stores = []
 if  out_path.exists():
     print("skipping scraping...")
 else:
@@ -26,39 +59,23 @@ else:
             start2 = start_timer()
             #search for the card name in the stores
             stores_data = get_stores_data(scraper.driver, wish_card_name)
+            
             mark_timer(start2, "scraping this page took:")
 
             #stores that have that card are updated/added
             for store in stores_data:
                 json_path = out_path / f"{sanitize_store_name(store.name or 'unknown')}.json"
-                stock = [asdict(item) for item in store.stock]
+                if not json_path.exists():
+                    all_stores.append(store)
 
-                
-                if json_path.exists():
-                    with open(json_path, "r", encoding="utf-8") as f:
-                        data = json.load(f)
-                        if stock:
-                            #if a store is called for having more than 1 card, inc their diversity
-                            data["diversity"] += 1
-                            #if it has the whole order os wish_deck, mark it
-                            # if has_the_whole_card_order(wish_card_name,wish_deck[wish_card_name], store):
-                            #     print("hello")
-
-                    data["stock"].extend(stock)
-
-                else:
-                    data ={
-                        "name": store.name,
-                        "address": store.address,
-                        "showcase_url": store.showcase_url,
-                        "diversity": 1,
-                        "stock": stock
-                    }
-                with open(json_path, "w", encoding="utf-8") as f:
-                    json.dump(data, f, indent=4, ensure_ascii=False)
+                store_data = write_json(store,json_path)
 
     mark_timer(start, "whole scraping took")
     scraper.close()
+
+sorted_stores =  sort_stores_by_diversity(all_stores)
+
+
 
 
 print("encerrando processo")
